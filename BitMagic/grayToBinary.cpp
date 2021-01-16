1988be5d68531c7bf9b43c31830da8960ebdf7f5
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-01-16 20:27:55 +0530

    Update grayToBinary.cpp

--- BitMagic/grayToBinary.cpp
+++ BitMagic/grayToBinary.cpp
@@ -1,5 +1,11 @@
 //https://practice.geeksforgeeks.org/problems/gray-to-binary-equivalent-1587115620/0
 int grayToBinary(int n)
 {
-    return n ^ (n >> 1); 
+    int inv = 0; 
+  
+    // Taking xor until n becomes zero 
+    for (; n; n = n >> 1) 
+        inv ^= n; 
+  
+    return inv;  
 }
