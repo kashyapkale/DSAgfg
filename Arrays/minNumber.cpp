58dbe6102e01061bf7d3803dc352dad3bd62143f
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-03-18 19:57:36 +0530

    Create minNumber.cpp

---
+++ Arrays/minNumber.cpp
@@ -0,0 +1,16 @@
+int minNumber(int arr[], int low, int high)
+{
+
+	if (arr[low] < arr[high])
+		return arr[low];
+
+	int mid = high + low ;
+	if (arr[mid] < arr[mid - 1])
+		return arr[mid];
+
+	if (arr[mid] > arr[0])
+		return minNumber(arr, mid + 1, high);
+
+	return minNumber(arr, low, mid - 1);
+
+}
