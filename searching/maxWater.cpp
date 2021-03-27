b1e41eab67e96dfd835726473ace4700c8a4ca6e
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-03-28 02:00:57 +0530

    Update maxWater.cpp

--- searching/maxWater.cpp
+++ searching/maxWater.cpp
@@ -4,7 +4,7 @@ int maxWater(int height[], int n)
 	int left = 0, right = n - 1;
 	int maximum = INT_MIN;
 	while (left <= right) {
-		int water = (right - left) * min(height[left], height[right]);
+		int water = (right - left - 1) * min(height[left], height[right]);
 		maximum = max(maximum, area);
 
 		if (height[right] < height[left])
