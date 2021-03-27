40396673b1ff1bbec585d54f7b8a42cdaa08eb05
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-03-28 01:58:36 +0530

    Create maxWater.cpp

---
+++ searching/maxWater.cpp
@@ -0,0 +1,17 @@
+//https://practice.geeksforgeeks.org/problems/maximum-water-between-two-buildings/0
+int maxWater(int height[], int n)
+{
+	int left = 0, right = n - 1;
+	int maximum = INT_MIN;
+	while (left <= right) {
+		int water = (right - left) * min(height[left], height[right]);
+		maximum = max(maximum, area);
+
+		if (height[right] < height[left])
+			right--;
+		else
+			left++;
+	}
+
+	return maximum;
+}
