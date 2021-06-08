be1e6623477e40e541c1154e7f3147a4d41f9d01
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-06-08 23:15:54 +0530

    Create heightOfBinaryTree.cpp

---
+++ Binary Tree/heightOfBinaryTree.cpp
@@ -0,0 +1,7 @@
+//https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+int height(struct Node* node){
+        if(!node)
+            return 0;
+            
+        return 1+max(height(node->left),height(node->right));
+    }
