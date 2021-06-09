940395a630c5729f2784ca9e1fdbacec5349b779
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-06-10 01:01:05 +0530

    Create topView.cpp

---
+++ Binary Tree/topView.cpp
@@ -0,0 +1,36 @@
+//https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1#
+vector<int> topView(Node *root)
+{
+	vector<int> ans;
+
+	if (!root)
+		return ans;
+
+	int maximum = INT_MIN;
+	int minimum = INT_MAX;
+	unordered_map<int, vector<int>> umap;
+	queue<pair<Node*, int>> q;
+	q.push(make_pair(root, 0));
+	while (!q.empty()) {
+		Node* temp = q.front().first;
+		int level = q.front().second;
+		if (temp->left)
+			q.push(make_pair(temp->left, level - 1));
+		if (temp->right)
+			q.push(make_pair(temp->right, level + 1));
+		umap[level].push_back(temp->data);
+		q.pop();
+
+		if (level > maximum)
+			maximum = level;
+
+		if (level < minimum)
+			minimum = level;
+	}
+
+	for (int i = minimum; i <= maximum; i++)
+		ans.push_back(umap[i][0]);
+
+
+	return ans;
+}
