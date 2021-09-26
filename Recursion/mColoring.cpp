eac2913c9a3f5e762fd384b9d0de254779e005e6
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-09-26 16:08:06 +0530

    Create mColoring.cpp

---
+++ Recursion/mColoring.cpp
@@ -0,0 +1,30 @@
+bool isSafe(int node, bool graph[101][101], int color[], int N, int color_Num){
+	for (int k = 0; k < N; k++) {
+		if (node != k && graph[k][node] == 1 && color[k] == color_Num)
+			return false;
+	}
+
+	return true;
+}
+
+bool solve(int node, bool graph[101][101], int color[], int m, int N){
+	if (node == N)
+		return true;
+
+	for (int i = 1; i <= m; i++) {
+		if (isSafe(node, graph, color, N, i)) {
+			color[node] = i;
+			if (solve(node + 1, graph, color, m, N))
+				return true;
+			color[node] = 0;
+		}
+	}
+
+	return false;
+}
+
+bool graphColoring(bool graph[101][101], int m, int N){
+	int color[N] = { 0 };
+
+	return solve(0, graph, color, m, N);
+}
