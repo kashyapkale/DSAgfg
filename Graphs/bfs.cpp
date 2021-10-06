88f9ceab6166aa273716a5d23f8c30e06b110f74
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2021-10-07 00:36:01 +0530

    Update bfs.cpp

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -1,7 +1,9 @@
 //bfs traversal of a graph
+
+//For graphs with only one component
 vector <int> bfsOfGraph(int N, vector<int> g[]) {
 	queue<int> q;
-// vector<bool> visited(N+1, false);
+	//vector<bool> visited(N+1, false);
 	bool *visited = new bool[N + 1] {false};
 	vector<int> ans;
 
@@ -17,4 +19,32 @@ vector <int> bfsOfGraph(int N, vector<int> g[]) {
 		q.pop();
 	}
 	return ans;
+}
+
+
+
+//For graphs with multiple components
+vector <int> bfsOfGraph(int N, vector<int> g[]) {
+	bool *visited = new bool[N + 1]{ false };
+	vector<int> ans;
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) {
+			queue<int> q;
+			q.push(i);
+			while (!q.empty()) {
+				for (auto node: g[q.front()]) {
+					if (!visited[node]) {
+						q.push(node);
+						visited[node] = true;
+					}
+				}
+				ans.push_back(q.front());
+				q.pop();
+			}
+		}
+	}
+
+	return ans;
+
 }
