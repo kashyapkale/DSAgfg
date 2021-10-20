6e8c0f697b5afc9a89a0eda5012dadc5ef7e9f6a
Kashyap Milind Kale <43263855+kashyapkale@users.noreply.github.com>
2021-10-20 23:32:45 +0530

    Create MSpanningTree.cpp

---
+++ Graphs/MSpanningTree.cpp
@@ -0,0 +1,40 @@
+//Minimum Spanning Tree - Prims Algorithm
+int spanningTree(int V, vector<vector<int> > adj[]){
+    vector<int> key(V + 1, INT_MAX);
+    vector<bool> mst(V + 1, false);
+    vector<int> parent(V + 1);
+
+    parent[0] = -1;
+    priority_queue <pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > minHeap;
+
+    key[0] = 0;
+    int ans = 0;
+
+    minHeap.push({ 0, 0 });
+    int i = 0;
+
+    while (!minHeap.empty()) {
+        int currNode = minHeap.top().second;
+        int currKey = minHeap.top().first;
+        ans += currKey;
+        //cout<<"For Itr :"<<i<<" ans:"<<ans<<" Curr Node:"<<currNode<<endl;
+        mst[currNode] = true;
+        minHeap.pop();
+        for (auto it : adj[currNode]) {
+            int nextNode = it[0];
+            int nextKey = it[1];
+            if (!mst[nextNode] && nextKey < key[nextNode]) {
+                parent[nextNode] = currNode;
+                key[nextNode] = nextKey;
+                minHeap.push({ nextKey, nextNode });
+            }
+        }
+        i++;
+    }
+    int sum = 0;
+
+    for (int i = 1; i <= V; i++) {
+        if (key[i] != INT_MAX) sum += key[i];
+    }
+    return sum;
+}
